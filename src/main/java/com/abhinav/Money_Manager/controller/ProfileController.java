1e10086722a19ff98ca019d7e6f95cb4e6447381
Abhinavan2004 <abhinavanpan@gmail.com>
2026-01-17 15:50:34 +0530

    Added registerProfile Logic

---
+++ src/main/java/com/abhinav/Money_Manager/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package com.abhinav.Money_Manager.controller;
+
+import com.abhinav.Money_Manager.dto.ProfileDTO;
+import com.abhinav.Money_Manager.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService ;
+
+    @PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO){
+        ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+    }
+
+}
